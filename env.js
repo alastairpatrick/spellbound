cf97c42be17f47a3ff02bd7f5abfd4fe27ead422
Al Patrick <Al Patrick>
2017-02-19 13:01:33 -0800

    env.js outputs NODE_ENV value to console

--- env.js
+++ env.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
-const NODE_ENV = process.env.NODE_ENV || 'production';
+let NODE_ENV;
+if (typeof process.env.NODE_ENV !== "string") {
+  NODE_ENV = "development";
+  console.log("NODE_ENV defaulting to '" + NODE_ENV + "'.");
+} else {
+  NODE_ENV = process.env.NODE_ENV;
+  console.log("NODE_ENV set to '" + NODE_ENV + "'.");
+}
+
 const IS_OPTIMIZED = NODE_ENV === 'production';
 const BUILD_DIR = 'build/';
 
